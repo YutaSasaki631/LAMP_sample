9e576d45a1486f9d6b0d64e64056b3daefce7d60
YutaSasaki631 <yuta.sasaki.sys19@morijyobi.ac.jp>
2020-06-22 11:52:39 +0900

    edit index.php

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 
 ?>
 
-<<<<<<< HEAD
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -94,18 +93,5 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-=======
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAMP TEST PAGE</title>
-</head>
-<body>
-  <?php foreach( $result as $value ){ ?>
-    <h1><?php echo "$value[name]<br>";?></h1>
-  <?php } ?>
->>>>>>> 2b8ae7bb0d12adb2d034ab7c071d6bf83ddfd934
 </body>
 </html>
